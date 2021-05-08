b3ada7b81121306a69b778f4d4a7750cac4bfc0b
Jesse Rutter <rutter1010@gmail.com>
2021-05-08 18:56:10 -0400

    moved app.js to the correct directory app.js is now running on localhost:3000

--- Develop/app.js
+++ Develop/app.js
@@ -19,9 +19,13 @@ app.use(express.urlencoded({ extended: true}))
 // All content in public folder is static content.
 app.use(express.static('public'))
 
+// On this '/' use the router and perform get
+app.use('/', router)
+app.use('/notes', router)
+
 // Setting up get requests for html routes (This is for main page of local host [Nothing after /])
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'))
+    res.sendFile(path.join(__dirname, 'public/'))
 })
 
 // Setting up get request for html routes (the html you get for /notes in URL.)
@@ -29,10 +33,11 @@ router.get('/notes', (req, res) =>{
     res.sendFile(path.join(__dirname, 'public/notes.html'))
 })
 
+// Setting up get request for html routes (the html you get for /api/notes in URL.)
 router.get('/api/notes', (req, res) => {
-    
+
 })
 
-app.listen(port, () => {
+app.listen(PORT, () => {
     console.log(`App Listening at http://localhost:${PORT}`)
 })
