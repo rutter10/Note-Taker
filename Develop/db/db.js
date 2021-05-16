6cb6ad5b84efa2e112458b14c8cd16779fea0b8e
Jesse Rutter <rutter1010@gmail.com>
2021-05-15 21:30:55 -0400

    Got undefined error to clear up. Add note, delete note functionality finally works

--- Develop/db/db.js
+++ Develop/db/db.js
@@ -1,45 +1,48 @@
 // built in to node => gives a few utilities.
-const util = require('util')
+const fs = require('fs');
+const util = require('util');
+const { v4: uuidv4 } = require('uuid');
 
-const fs = require('fs')
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
 
-const { v4: uuidv4 } = require('uuid')
-
-const readFile = util.promisify(fs.readFile)
-const writeFile = util.promisify(fs.writeFile)
-   
+// Creating a class for DB
 class Database {
-    async getAllNotes() {
-        return readFile('db/db.json', {encoding: 'utf8'}).then((notes) => {
-            try{
-                return JSON.parse(notes)
-            }   catch (error) {
-                return JSON.parse([])
-            }
-        })
+  // Method to getAllNotes
+  async getAllNotes() {
+    return readFile('db/db.json', { encoding: 'utf8' }).then((notes) => {
+      try {
+        return JSON.parse(notes);
+      } catch (error) {
+        return JSON.parse([]);
+      }
+    });
+  }
+
+  // Method to addNote
+  async addNote({ title, text }) {
+
+    if (!title || !text) {
+      throw new Error("Note 'title' and 'text' cannot be blank");
     }
 
-    async addNotes ({ title, text}) {
+    const note = { title, text, id: uuidv4() };
 
-        if (!title || !text) {
-            throw new Error ("Note 'title' and 'text' cannot be blank")
-        }
+    let allNotes = await this.getAllNotes();
+    allNotes = [].concat(allNotes, note);
+    console.log(allNotes);
+    writeFile('db/db.json', JSON.stringify(allNotes));
+    return allNotes;
+  }
 
-        const note = {title, text, id: uuidv4() }
-
-        let allNotes = await this.getAllNotes()
-        allNotes = [].concat(allNotes, note)
-        console.log(allNotes)
-        writeFile('db/db.json', JSON.stringify(allNotes))
-        return allNotes
-    }
-
-    async deleteNote(id) {
-        console.log(id)
-        let allNotes = await this.getAllNotes()
-        allNotes = allNotes.filter((note) => note.id !== id)
-        writeFile('db/db.json', JSON.stringify(allNotes))
-    }
+  // Method to deleteNote
+  async deleteNote(id) {
+    console.log(id);
+    let allNotes = await this.getAllNotes();
+    allNotes = allNotes.filter((note) => note.id !== id);
+    writeFile('db/db.json', JSON.stringify(allNotes));
+    return allNotes;
+  }
 }
 
 module.exports = new Database();
